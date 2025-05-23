3187500214f573c62ed552f4de878448bb82aa6a
chennakrishnadevops <chennakrishna.devops17@gmail.com>
2025-05-23 20:32:00 +0530

    deleted unnessary file

--- carrent.py
+++
@@ -1,8 +0,0 @@
-age = int(input("Enter customer age: "))
-if age < 18:
-    print("cannot rent a car")
-else:
-    if 18 <= age <= 25:
-        print("Pay an additional surcharge")
-    else:
-        print("No Surcharge")
